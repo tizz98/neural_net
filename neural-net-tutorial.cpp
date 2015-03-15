569648fbf08edd8e9f490721410d5168db43da7e
tizz98 <dev.tizz98@gmail.com>
2015-03-14 20:54:32 -0400

    added variable types

--- neural-net-tutorial.cpp
+++ neural-net-tutorial.cpp
@@ -1,21 +1,30 @@
 //neural-net-tutorial.cpp
 
+#include <vector>
+
 class Net
 {
 public:
-	Net(topology);
-	void feedForward(inputVals);
-	void backProp(targetVals);
-	void getResults(resultVals) const;
+	Net(const std::vector<unsigned> &topology);
+	void feedForward(const std::vector<double> &inputVals);
+	void backProp(const std::vector<double> &targetVals);
+	void getResults(std::vector<double> &resultVals) const;
 
 private:
 };
 
 int main()
 {
+	// eg., {3, 2, 1}
+	std::vector<unsigned> topology;
 	Net myNet(topology);
 
+	std::vector<double> inputVals;
 	myNet.feedForward(inputVals);
+
+	std::vector<double> targetVals;
 	myNet.backProp(targetVals);
+
+	std::vector<double> resultVals;
 	myNet.getResults(resultVals);
 }
