fdfd4582f64d11defd140f2bbb0bba80da8993f9
tizz98 <dev.tizz98@gmail.com>
2015-03-14 22:05:28 -0400

    added traningdata class

--- neural-net-tutorial.cpp
+++ neural-net-tutorial.cpp
@@ -5,6 +5,89 @@
 #include <cstdlib>
 #include <cassert>
 #include <cmath>
+#include <fstream>
+#include <sstream>
+
+class TrainingData
+{
+public:
+    TrainingData(const string filename);
+    bool isEof(void) { return m_trainingDataFile.eof(); }
+    void getTopology(vector<unsigned> &topology);
+
+    // Returns the number of input values read from the file:
+    unsigned getNextInputs(vector<double> &inputVals);
+    unsigned getTargetOutputs(vector<double> &targetOutputVals);
+
+private:
+    ifstream m_trainingDataFile;
+};
+
+void TrainingData::getTopology(vector<unsigned> &topology)
+{
+    string line;
+    string label;
+
+    getline(m_trainingDataFile, line);
+    stringstream ss(line);
+    ss >> label;
+    if (this->isEof() || label.compare("topology:") != 0) {
+        abort();
+    }
+
+    while (!ss.eof()) {
+        unsigned n;
+        ss >> n;
+        topology.push_back(n);
+    }
+
+    return;
+}
+
+TrainingData::TrainingData(const string filename)
+{
+    m_trainingDataFile.open(filename.c_str());
+}
+
+unsigned TrainingData::getNextInputs(vector<double> &inputVals)
+{
+    inputVals.clear();
+
+    string line;
+    getline(m_trainingDataFile, line);
+    stringstream ss(line);
+
+    string label;
+    ss>> label;
+    if (label.compare("in:") == 0) {
+        double oneValue;
+        while (ss >> oneValue) {
+            inputVals.push_back(oneValue);
+        }
+    }
+
+    return inputVals.size();
+}
+
+unsigned TrainingData::getTargetOutputs(vector<double> &targetOutputVals)
+{
+    targetOutputVals.clear();
+
+    string line;
+    getline(m_trainingDataFile, line);
+    stringstream ss(line);
+
+    string label;
+    ss>> label;
+    if (label.compare("out:") == 0) {
+        double oneValue;
+        while (ss >> oneValue) {
+            targetOutputVals.push_back(oneValue);
+        }
+    }
+
+    return targetOutputVals.size();
+}
 
 struct Connection
 {
@@ -160,6 +243,7 @@ Net::Net(const std::vector<unsigned> &topology)
 			m_layers.back().push_back(Neuron(numOutputs));
 			std::cout << "Made a Neuron!" << std::endl;
 		}
+		
 	}
 }
 
@@ -240,21 +324,55 @@ void Net::getResults(std::vector<double> &resultVals) const
 	}
 }
 
+void showVectorVals(string label, vector<double> &v)
+{
+    cout << label << " ";
+    for (unsigned i = 0; i < v.size(); ++i) {
+        cout << v[i] << " ";
+    }
+
+    cout << endl;
+}
+
 int main()
 {
-	// eg., {3, 2, 1}
-	std::vector<unsigned> topology;
-	topology.push_back(3);
-	topology.push_back(2);
-	topology.push_back(1);
-	Net myNet(topology);
+    TrainingData trainData("/tmp/trainingData.txt");
 
-	std::vector<double> inputVals;
-	myNet.feedForward(inputVals);
+    // e.g., { 3, 2, 1 }
+    vector<unsigned> topology;
+    trainData.getTopology(topology);
 
-	std::vector<double> targetVals;
-	myNet.backProp(targetVals);
+    Net myNet(topology);
 
-	std::vector<double> resultVals;
-	myNet.getResults(resultVals);
+    vector<double> inputVals, targetVals, resultVals;
+    int trainingPass = 0;
+
+    while (!trainData.isEof()) {
+        ++trainingPass;
+        std::cout << std::endl << "Pass " << trainingPass;
+
+        // Get new input data and feed it forward:
+        if (trainData.getNextInputs(inputVals) != topology[0]) {
+            break;
+        }
+        showVectorVals(": Inputs:", inputVals);
+        myNet.feedForward(inputVals);
+
+        // Collect the net's actual output results:
+        myNet.getResults(resultVals);
+        showVectorVals("Outputs:", resultVals);
+
+        // Train the net what the outputs should have been:
+        trainData.getTargetOutputs(targetVals);
+        showVectorVals("Targets:", targetVals);
+        assert(targetVals.size() == topology.back());
+
+        myNet.backProp(targetVals);
+
+        // Report how well the training is working, average over recent samples:
+        std::cout << "Net recent average error: "
+                << myNet.getRecentAverageError() << std::endl;
+    }
+
+    std::cout << std::endl << "Done" << std::endl;
 }
